c5f93bd34a6aee4b22a8e55bdfefc3735d17b4ef
jayantkeswani <keswani.jayant@yahoo.com>
2017-04-27 14:08:03 -0700

    Update

--- server/controller/crons.js
+++ server/controller/crons.js
@@ -5,95 +5,117 @@ exports.crons = function(request, response) {
     return_respose(request.body);
 
     function return_respose(body) {
-        calendar = {
-            Monthly: [],
-            Weekly: [],
-            BiWeekly: [],
-            Quarterly: []
-        };
+        objects = get_bjects();
+        calendar = objects[0];
+        response_json = objects[1];
+        options = objects[2];
 
-        response_json = {
-            Monthly: [],
-            Weekly: [],
-            BiWeekly: [],
-            Quarterly: []
-        };
-        var start_date = new Date(body.start_date);
-        var end_date = new Date(body.end_date);
-        var options = {
-            currentDate: new Date(start_date),
-            endDate: new Date(end_date),
-            iterator: true
-        };
+        calendar_create();
+        process_corn_strings();
+        Sorting();
+
+        // Return Calendar, response and options object
+        function get_bjects() {
+
+            var calendar = {
+                Monthly: [],
+                Weekly: [],
+                BiWeekly: [],
+                Quarterly: []
+            };
+
+            var response_json = {
+                Monthly: [],
+                Weekly: [],
+                BiWeekly: [],
+                Quarterly: []
+            };
+
+            var start_date = new Date(body.start_date);
+            var end_date = new Date(body.end_date);
+
+            var options = {
+                currentDate: new Date(start_date),
+                endDate: new Date(end_date),
+                iterator: true
+            };
+            return [calendar, response_json, options];
+        }
 
         // Processing response body to create an Object (Calendar)
-        for (var i in body.frequencies) {
-            if (body.frequencies[i].name == 'Monthly') {
-                for (var m in body.frequencies[i].crons) {
-                    if (calendar.Monthly.indexOf(body.frequencies[i].crons[m]) < 0) {
-                        calendar.Monthly.push(body.frequencies[i].crons[m]);
+        function calendar_create() {
+            for (var i in body.frequencies) {
+                if (body.frequencies[i].name == 'Monthly') {
+                    for (var m in body.frequencies[i].crons) {
+                        if (calendar.Monthly.indexOf(body.frequencies[i].crons[m]) < 0) {
+                            calendar.Monthly.push(body.frequencies[i].crons[m]);
+                        }
                     }
-                }
-            } else if (body.frequencies[i].name == 'Weekly') {
-                for (var w in body.frequencies[i].crons) {
-                    if (calendar.Weekly.indexOf(body.frequencies[i].crons[w]) < 0) {
-                        calendar.Weekly.push(body.frequencies[i].crons[w]);
+                } else if (body.frequencies[i].name == 'Weekly') {
+                    for (var w in body.frequencies[i].crons) {
+                        if (calendar.Weekly.indexOf(body.frequencies[i].crons[w]) < 0) {
+                            calendar.Weekly.push(body.frequencies[i].crons[w]);
+                        }
                     }
-                }
-            } else if (body.frequencies[i].name == 'BiWeekly') {
-                for (var b in body.frequencies[i].crons) {
-                    if (calendar.BiWeekly.indexOf(body.frequencies[i].crons[b]) < 0) {
-                        calendar.BiWeekly.push(body.frequencies[i].crons[b]);
+                } else if (body.frequencies[i].name == 'BiWeekly') {
+                    for (var b in body.frequencies[i].crons) {
+                        if (calendar.BiWeekly.indexOf(body.frequencies[i].crons[b]) < 0) {
+                            calendar.BiWeekly.push(body.frequencies[i].crons[b]);
+                        }
                     }
-                }
-            } else {
-                for (var q in body.frequencies[i].crons) {
-                    if (calendar.Quarterly.indexOf(body.frequencies[i].crons[q]) < 0) {
-                        calendar.Quarterly.push(body.frequencies[i].crons[q]);
+                } else {
+                    for (var q in body.frequencies[i].crons) {
+                        if (calendar.Quarterly.indexOf(body.frequencies[i].crons[q]) < 0) {
+                            calendar.Quarterly.push(body.frequencies[i].crons[q]);
+                        }
                     }
                 }
             }
         }
 
         // Processing cron strings
-        for (var cal in calendar) {
-            for (var k in calendar[cal]) {
-                try {
-                    var interval = parser.parseExpression(calendar[cal][k], options);
-                    while (true) {
-                        try {
-                            var obj = interval.next();
-                            if (cal == 'Monthly') {
-                                response_json.Monthly.push(obj.value.toString());
-                            } else if (cal == 'Weekly') {
-                                response_json.Weekly.push(obj.value.toString());
-                            } else if (cal == 'BiWeekly') {
-                                response_json.BiWeekly.push(obj.value.toString());
-                            } else {
-                                response_json.Quarterly.push(obj.value.toString());
+        function process_corn_strings() {
+            for (var cal in calendar) {
+                for (var k in calendar[cal]) {
+                    try {
+                        var interval = parser.parseExpression(calendar[cal][k], options);
+                        while (true) {
+                            try {
+                                var obj = interval.next();
+                                if (cal == 'Monthly') {
+                                    response_json.Monthly.push(obj.value.toString());
+                                } else if (cal == 'Weekly') {
+                                    response_json.Weekly.push(obj.value.toString());
+                                } else if (cal == 'BiWeekly') {
+                                    response_json.BiWeekly.push(obj.value.toString());
+                                } else {
+                                    response_json.Quarterly.push(obj.value.toString());
+                                }
+                            } catch (e) {
+                                break;
                             }
-                        } catch (e) {
-                            break;
                         }
+                    } catch (err) {
+                        console.log('Error: ' + err.message);
                     }
-
-                } catch (err) {
-                    console.log('Error: ' + err.message);
                 }
             }
         }
 
         //Sorting and conversion of TimeStamps to ISO-8601
-        for (var l in response_json) {
-            response_json[l].sort(function(a, b) {
-                return a.split(" ")[2] - b.split(" ")[2];
-            });
-            for (var p in response_json[l]) {
-                response_json[l][p] = new Date(response_json[l][p]).toISOString();
+        function Sorting() {
+            for (var l in response_json) {
+                response_json[l].sort(function(a, b) {
+                    return a.split(" ")[2] - b.split(" ")[2];
+                });
+                for (var p in response_json[l]) {
+                    response_json[l][p] = new Date(response_json[l][p]).toISOString();
+                }
             }
         }
 
-        //Send Response
+    //Send Response
         response.send(response_json);
+
     }
-}
+};
